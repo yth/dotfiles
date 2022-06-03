5dd008118d9c3a11d9f0a7c1c0d44b718b96b451
Yuan Cao <yuan.cao.email@gmail.com>
2022-06-03 12:12:51 -0400

    add simple emacs config file

---
+++ init.el
@@ -0,0 +1,33 @@
+(global-display-line-numbers-mode)
+(global-tab-line-mode)
+
+(setq inhibit-startup-screen t)
+
+(add-hook 'emacs-startup-hook 'treemacs)
+
+(require 'package)
+(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/")))
+
+(load-theme 'darcula t)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("79586dc4eb374231af28bbc36ba0880ed8e270249b07f814b0e6555bdcb71fab" default))
+ '(package-selected-packages '(tuareg treemacs-all-the-icons sml-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(global-set-key (kbd "C-c t") 'treemacs)
+
+;; ## added by OPAM user-setup for emacs / base ## 56ab50dc8996d2bb95e7856a6eddb17b ## you can edit, but keep this line
+(require 'opam-user-setup "~/.emacs.d/opam-user-setup.el")
+;; ## end of OPAM user-setup addition for emacs / base ## keep this line
